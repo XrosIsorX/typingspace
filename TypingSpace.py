851638ca79a89a5895cb94902c9ab85d205c7a2c
XrosIsorX <olantoj@hotmail.com>
2016-12-22 21:42:05 +0700

    can render block

--- TypingSpace.py
+++ TypingSpace.py
@@ -27,15 +27,21 @@ class SpaceGameWindow(arcade.Window):
 
         self.world = World(width, height)
         self.background_texture = arcade.load_texture('images/backgroundSpace.jpg')
+        self.stamina_block_texture = arcade.load_texture('images/staminaBlock.png')
+        self.defense_block_texture = arcade.load_texture('images/defenseBlock.png')
 
         self.spacecraft_sprite = []
         for spacecraft in self.world.enemys:
             self.spacecraft_sprite = ModelSprite('images/ship.png', model = spacecraft)
 
+    def draw_block(self):
+        arcade.draw_texture_rectangle(675, 150 / 2, 450, 150, self.defense_block_texture)
+        arcade.draw_texture_rectangle(450 / 2, 150 / 2, 450, 150, self.stamina_block_texture)
+
     def on_draw(self):
         arcade.start_render()
-        arcade.draw_texture_rectangle(640, 360, 1280, 720,
-                                              self.background_texture)
+        arcade.draw_texture_rectangle(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, SCREEN_WIDTH, SCREEN_HEIGHT, self.background_texture)
+        self.draw_block()
         self.spacecraft_sprite.draw()
 
     def animate(self, delta_time):
