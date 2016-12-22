db63539dcf11f635c98695091df0152ab39ac498
XrosIsorX <olantoj@hotmail.com>
2016-12-23 01:55:46 +0700

    restructure and can spawn ship

--- TypingSpace.py
+++ TypingSpace.py
@@ -26,32 +26,28 @@ class SpaceGameWindow(arcade.Window):
         arcade.set_background_color(arcade.color.BLACK)
 
         self.world = World(width, height)
+
         self.background_texture = arcade.load_texture('images/backgroundSpace.jpg')
-        self.stamina_block_texture = arcade.load_texture('images/staminaBlock.png')
-        self.defense_block_texture = arcade.load_texture('images/defenseBlock.png')
+        self.my_ship_texture = arcade.load_texture('images/myShip.png')
+        self.enemy_texture = arcade.load_texture('images/ship.png')
 
-        self.spacecraft_sprite = []
-        for spacecraft in self.world.enemys:
-            self.spacecraft_sprite = ModelSprite('images/ship.png', model = spacecraft)
+    def draw_enemy(self):
+        for ship in self.world.enemys:
+            arcade.draw_texture_rectangle(ship.x, ship.y, ship.width, ship.height, self.enemy_texture)
 
-    def draw_block(self):
-        arcade.draw_texture_rectangle(675, 150 / 2, 450, 150, self.defense_block_texture)
-        arcade.draw_texture_rectangle(450 / 2, 150 / 2, 450, 150, self.stamina_block_texture)
+    def draw_status(self):
+            arcade.draw_text(str(self.world.count_time), self.width - 550, self.height - 30, arcade.color.WHITE, 20)
 
     def on_draw(self):
         arcade.start_render()
         arcade.draw_texture_rectangle(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, SCREEN_WIDTH, SCREEN_HEIGHT, self.background_texture)
-        self.draw_block()
-        self.spacecraft_sprite.draw()
+        arcade.draw_texture_rectangle(SCREEN_WIDTH / 2, 75, SCREEN_WIDTH, 150, self.my_ship_texture)
+
+        self.draw_enemy()
+        self.draw_status()
 
     def animate(self, delta_time):
         self.world.animate(delta_time)
-
-    def on_key_press(self, key, key_modifiers):
-        self.world.on_key_press(key, key_modifiers)
-
-    def on_key_release(self, key, key_modifiers):
-        self.world.on_key_release(key, key_modifiers)
 
 if __name__ == '__main__':
     window = SpaceGameWindow(SCREEN_WIDTH, SCREEN_HEIGHT)
--- model.py
+++ model.py
@@ -10,29 +10,41 @@ class Model:
         self.x = x
         self.y = y
 
-class typingWord(Model):
-    def __init__(self, world, x, y, word):
+class Spacecraft(Model):
+    def __init__(self, world, x, y, width, height):
         super().__init__(world, x, y)
-        self.word = word
-        self.index = 0
-
-class Spacecraft(typingWord):
-    def __init__(self, world, x, y, word):
-        super().__init__(world, x, y, word)
+        self.width = width
+        self.height = height
 
     def shot(self):
         self.x = 0
+
+class typingWord():
+    def __init__(self, world, word):
+        self.word = word
+        self.index = 0
 
 class World:
     def __init__(self, width, height):
         self.width = width
         self.height = height
 
-        self.mode = []
-        self.mode.append(typingWord(self, 100, 100, "stamina"))
-        self.mode.append(typingWord(self, 200, 100, "defense"))
+        self.count_time = 0
+
         self.enemys = []
-        self.enemys.append(Spacecraft(self, 100, 100, "Hello"))
+        self.typing_word = typingWord(self, 'ABC')
+
+    # def update_word(self):
+    #     if
+    # # def typing(self):
+    # #
+
+    def spawn_ship(self):
+        if self.count_time > 1:
+            self.enemys.append(Spacecraft(self, randint(0, 900), randint(400, 600), 64 ,128))
+            self.count_time = 0
 
     def animate(self, delta_time):
-        self.x = delta_time
+        self.spawn_ship()
+
+        self.count_time += delta_time
